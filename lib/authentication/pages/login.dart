1b2a0e4fd940ac6545464da3df03f4ef8fe49613
mhmdhaekal <muhammad.haekal22@ui.ac.id>
2023-12-15 00:57:17 +0700

    done register page

---
+++ lib/authentication/pages/login.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  late Dio dio;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+    );
+  }
+}
