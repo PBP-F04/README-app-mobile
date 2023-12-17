7be5368dd8a65491fcbcb7ecd129b527eb01d61d
shanahandnlf <shanahan.danualif@ui.ac.id>
2023-12-18 04:46:30 +0700

    fix routing main.dart

--- lib/forum_diskusi/pages/discussion_forum.dart
+++ lib/forum_diskusi/pages/discussion_forum.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:readme_mobile/forum_diskusi/pages/discussion_comment.dart';
 import 'dart:convert';
 
 // import 'package:hanshop/models/item.dart';
@@ -9,6 +10,8 @@ import '../models/discussion.dart';
 // import 'detail_item.dart';
 
 class DiscussionForumPage extends StatefulWidget {
+  // final Book book; // from katalog
+
   const DiscussionForumPage({Key? key}) : super(key: key);
 
   @override
@@ -19,8 +22,7 @@ class _DiscussionForumPageState extends State<DiscussionForumPage> {
   Future<List<Discussion>> fetchProduct() async {
     // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
     //"https://shanahan-danualif-tugas.pbp.cs.ui.ac.id/json/"
-    var url = Uri.parse(
-        'http://127.0.0.1:8000/discussions/json-discussions/');
+    var url = Uri.parse('http://127.0.0.1:8000/discussions/json-discussions/');
     var response = await http.get(
       url,
       headers: {"Content-Type": "application/json"},
@@ -58,7 +60,7 @@ class _DiscussionForumPageState extends State<DiscussionForumPage> {
                       Text(
                         "Tidak ada data Discussion.",
                         style:
-                        TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                            TextStyle(color: Color(0xff59A5D8), fontSize: 20),
                       ),
                       SizedBox(height: 8),
                     ],
@@ -67,41 +69,41 @@ class _DiscussionForumPageState extends State<DiscussionForumPage> {
                   return ListView.builder(
                       itemCount: snapshot.data!.length,
                       itemBuilder: (_, index) => Container(
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 12),
-                        padding: const EdgeInsets.all(20.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "${index + 1}.${snapshot.data![index].fields.title}",
-                              style: const TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
-                              ),
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 12),
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "${index + 1}.${snapshot.data![index].fields.title}",
+                                  style: const TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                // Text("${snapshot.data![index].fields.amount}"),
+                                // const SizedBox(height: 10),
+                                Text("${snapshot.data![index].fields.content}"),
+                                const SizedBox(height: 10),
+                                // Text("${snapshot.data![index].fields.price}"),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => CommentPage(
+                                            discussion: snapshot.data![index]),
+                                      ),
+                                    );
+                                  },
+                                  child: const Text('Detail Discussion'),
+                                ),
+                              ],
                             ),
-                            const SizedBox(height: 10),
-                            // Text("${snapshot.data![index].fields.amount}"),
-                            // const SizedBox(height: 10),
-                            Text(
-                                "${snapshot.data![index].fields.content}"),
-                            const SizedBox(height: 10),
-                            // Text("${snapshot.data![index].fields.price}"),
-                            // ElevatedButton(
-                            //   onPressed: () {
-                            //     Navigator.push(
-                            //       context,
-                            //       MaterialPageRoute(
-                            //         builder: (context) => DetailDiscussionPage(discussion: snapshot.data![index]),
-                            //       ),
-                            //     );
-                            //   },
-                            //   child: const Text('Detail Discussion'),
-                            // ),
-                          ],
-                        ),
-                      ));
+                          ));
                 }
               }
             }));
--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       navigatorKey: navigatorKey,
       title: 'README',
-      initialRoute: '/',
+      initialRoute: '/discussion-forum',
       routes: {
         '/': (context) => const LoginPage(),
         '/register': (context) => const RegisterPage(),
         '/discussion-forum' : (context) => const DiscussionForumPage(),
-        '/discussion-comment' : (context) => const CommentPage(),
+        // '/discussion-comment' : (context) => const CommentPage(),
       },
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
